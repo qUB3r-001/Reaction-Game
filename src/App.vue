42e5da7fd28fd54a877b775013501a86fdb91c2e
Sid250101 <sid25porwal@gmail.com>
2021-02-04 19:35:41 +0530

    commit 1 basics for vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>REACTION TEST</h1>
+    <Target />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Target from "./components/Target";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "App",
+    components: { Target },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    text-align: center;
+    border: 1px solid black;
+  }
 </style>
---
+++ src/components/Target.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>Time : {{ count }}</h1>
+    <div
+      class="circle"
+      v-on:click="stopTimer"
+      v-if="hide"
+      v-bind:style="yellow"
+    ></div>
+    <button v-on:click="timer" v-bind:disabled="disable">Start</button>
+    <button v-on:click="reset">Reset</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Target",
+    data() {
+      return {
+        hide: true,
+        count: 0,
+        rsTimer: null,
+        disable: false,
+      };
+    },
+    methods: {
+      stopTimer() {
+        this.hide = false;
+        clearInterval(this.rsTimer);
+      },
+      timer() {
+        this.disable = true;
+        this.rsTimer = setInterval(() => {
+          this.count = this.count + 10;
+        }, 10);
+      },
+      reset() {
+        this.hide = true;
+        this.count = 0;
+        this.disable = false;
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .circle {
+    height: 100px;
+    width: 100px;
+    border-radius: 50px;
+    background-color: red;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
